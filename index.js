9a4bd0388ac395f355967b692121662003bf5d03
Andrew Braza <andrew.braza@wnu.sti.edu.ph>
2026-08-05 09:05:38 +0800

    Move server.js to api/index.js for complete Vercel zero-config compatibility

--- index.js
+++
@@ -1,5 +0,0 @@
-// Mustang Esports Entrypoint for Cloud Deployment Platforms (Vercel, Render, Heroku)
-const express = require('express');
-const app = require('./server.js');
-
-module.exports = app;
